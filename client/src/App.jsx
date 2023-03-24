c6d31cc759cf73e766d756ab6bb0c539a5962f68
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-03-24 13:05:25 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dashboard, Landing, Register, Login, ErrorPage } from "./pages/export";
+import { Dashboard, Landing, Register, ErrorPage } from "./pages/export";
 
 const App = () => {
   return (
@@ -8,7 +8,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>

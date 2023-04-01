a5018c4988b25b253c4f585259d55702c95feb18
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-04-01 11:53:58 +0530

    11:55am

--- client/src/context/appContext.jsx
+++ client/src/context/appContext.jsx
@@ -15,6 +15,9 @@ import {
   SETUP_USER_ERROR,
   TOGGLE_SIDEBAR,
   LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR,
 } from "./actions";
 
 const user = localStorage.getItem("user");
@@ -66,9 +69,9 @@ const AppProvider = ({ children }) => {
       return response;
     },
     (error) => {
-      console.log(error.response);
+      // console.log(error.response);
       if (error.response.status === 401) {
-        console.log("AUTH ERROR");
+        logoutUser();
       }
       return Promise.reject(error);
     }
@@ -183,12 +186,26 @@ const AppProvider = ({ children }) => {
 
   //* update user
   const updateUser = async (currentUser) => {
+    dispatch({ type: UPDATE_USER_BEGIN });
+
     try {
       const { data } = await authFetch.patch("/auth/updateUser", currentUser);
-      console.log(data);
+      const { user, token, location } = data;
+      dispatch({
+        type: UPDATE_USER_SUCCESS,
+        payload: { user, token, location },
+      });
+      //$ localstorage
+      addUserToLocalStorage({ user, token, location });
     } catch (error) {
-      console.log(error.response);
+      if (error.response.status !== 401) {
+        dispatch({
+          type: UPDATE_USER_ERROR,
+          payload: { msg: error.response.data.msg },
+        });
+      }
     }
+    clearAlert();
   };
 
   return (

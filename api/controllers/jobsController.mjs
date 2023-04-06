d30f255a448354f3ec1108e36ccde5db96dfac92
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-04-06 18:32:03 +0530

    06:31pm

--- api/controllers/jobsController.mjs
+++ api/controllers/jobsController.mjs
@@ -4,6 +4,7 @@ import { BadRequestError, NotFoundError } from "../errors/export.js";
 import checkPermissions from "../utils/checkPermissions.mjs";
 import mongoose from "mongoose";
 const { Types } = mongoose;
+import moment from "moment";
 
 //* Create job
 const createJob = async (req, res) => {
@@ -79,9 +80,35 @@ const showStats = async (req, res) => {
     declined: stats.declined || 0,
   };
 
-  let monthlyApplications = [];
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
 
-  res.status(StatusCodes.OK).json({  defaultStats, monthlyApplications });
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+
+      return { date, count };
+    })
+    .reverse();
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 export { createJob, deleteJob, getAllJobs, updateJob, showStats };

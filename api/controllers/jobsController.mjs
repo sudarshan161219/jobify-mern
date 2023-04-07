98db1c05f60a6ef9c98a0ddf7c63a623696cb6c2
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-04-07 19:56:09 +0530

    07:57pm

--- api/controllers/jobsController.mjs
+++ api/controllers/jobsController.mjs
@@ -52,11 +52,19 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
+  //$ setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page -1) * limit;
+  result = result.skip(skip).limit(limit)
+
   const jobs = await result;
 
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numofPages = Math.ceil(totalJobs / limit)
   return res
     .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numofPages: 1 });
+    .json({ jobs, totalJobs, numofPages});
 };
 
 // * Update Job
